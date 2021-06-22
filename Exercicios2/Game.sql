88591996cac4cdb00061d23adf706465e975d97b
Gabriella <85173132+GabriellaLopess@users.noreply.github.com>
2021-06-22 15:08:07 -0300

    Update Game.sql

--- Exercicios2/Game.sql
+++ Exercicios2/Game.sql
@@ -43,6 +43,6 @@ select* from tb_personagem where ataque > 2000;
 
 select* from tb_personagem where defesa between 1000 and 2000;
 
-select* from tb_personagem where nome like "%c";
+select* from tb_personagem where nome like "%c%";
 
 select * from tb_personagem inner join tb_classe on tb_classe.id = tb_personagem.classe_id;
