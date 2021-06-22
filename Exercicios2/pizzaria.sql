8062a3910666babb1f515c26245c3c1a33b3c33a
Gabriella <85173132+GabriellaLopess@users.noreply.github.com>
2021-06-22 15:09:06 -0300

    Update pizzaria.sql

--- Exercicios2/pizzaria.sql
+++ Exercicios2/pizzaria.sql
@@ -40,6 +40,6 @@ select* from tb_pizza where preco > 45.00;
 
 select* from tb_pizza where preco between 29.00 and 60;
 
-select* from tb_pizza where sabor like "%c";
+select* from tb_pizza where sabor like "%c%";
 
 select * from tb_pizza inner join tb_categoria on tb_categoria.id = tb_pizza.categoria_id;
